05be89d2bbf560eb77eef6e7a540cd888761a3bd
Thibaud Fresnay <thibaudfresnay.pro@gmail.com>
2022-12-07 12:09:48 +0100

    I finished the day 2 of 2019 and almost finished the day 2 of 2018

--- app.js
+++ app.js
@@ -19,9 +19,12 @@ import dayfunction_1_2_2017 from "./years/2017/day/day1.2.js";
 //2018
 import dayfunction_1_2018 from "./years/2018/day/day1.js";
 import dayfunction_1_2_2018 from "./years/2018/day/day1.2.js";
+import dayfunction_2_2018 from "./years/2018/day/day2.js";
 //2019
 import dayfunction_1_2019 from "./years/2019/day/day1.js";
 import dayfunction_1_2_2019 from "./years/2019/day/day1.2.js";
+import dayfunction_2_2019 from "./years/2019/day/day2.js";
+
 //2020
 import dayfunction_1_2020 from "./years/2020/day/day1.js";
 import dayfunction_1_2_2020 from "./years/2020/day/day1.2.js";
@@ -39,4 +42,4 @@ import dayfunction_2_2022 from "./years/2022/day/day2.js";
 import dayfunction_2_2_2022 from "./years/2022/day/day2.2.js";
 
 
-console.log(dayfunction_2_2_2020());
+console.log(dayfunction_2_2018());
