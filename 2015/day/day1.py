1da9e036993d70c7f83f533d86d6a4adcfa3aa6f
Thibaud <thibaudfresnay.pro@gmail.com>
2024-12-27 11:41:37 +0100

    code refactoried

--- 2015/day/day1.py
+++ 2015/day/day1.py
@@ -1,6 +1,41 @@
 file = open("./2015/in/day1.txt", "r")
-content = file.read()
+movesList = file.read()
 
-print(content)
+test1 = "(())" #0
+test2 = "()()" #0
+test3 = "(((" #3  
+test4 = "(()(()(" #3
+test5 = "))(((((" #3
+test6 = "())" #-1
+
+
+
+def addSubtractFloor (actual_floor, bracket):
+    if bracket == "(":
+        return actual_floor + 1
+    elif bracket == ")":
+        return actual_floor - 1
+    else:
+        return "wrong input"
+    
+def calculateFinalFloor(moves):
+    movesCopy = list(moves)
+    actual_floor = 0
+
+    for move in movesCopy:
+        actual_floor = addSubtractFloor(actual_floor, move)
+
+    return actual_floor
+
+result = calculateFinalFloor(movesList)
+
+print("test1", calculateFinalFloor(test1))
+print("test2", calculateFinalFloor(test2))
+print("test3", calculateFinalFloor(test3))
+print("test4", calculateFinalFloor(test4))
+print("test5", calculateFinalFloor(test5))
+print("test6", calculateFinalFloor(test6))
+print("moves", result)
 
 file.close()
+
