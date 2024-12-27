4c4cdc25c62f7a227fadc29dc912e9cd4573a99c
Thibaud <thibaudfresnay.pro@gmail.com>
2024-12-27 15:39:50 +0100

    day2 resolved

--- 2015/day/day2.py
+++ 2015/day/day2.py
@@ -2,24 +2,26 @@ import re
 
 file = open("./2015/in/day2.txt", "r")
 dimensionsList = re.split("\s", file.read())
+test1 = ["2x3x4","1x1x10"] #58+  43 = 101
 
-totalSquareFeet = 0
+def calculatetotalSquareFeet(dimensions: list[str]) -> int:
+    totalSquareFeet = 0
 
-def calculateSurfAreaExtra(area: int, smallSide: int) -> int:
-    return area + smallSide
+    for dimension in dimensions:
+        length, width, height = re.split("x", dimension)
+        
+        side1 = int(length) * int(width)
+        side2 = int(width) * int(height)
+        side3 = int(height) * int(length)
 
-for dimension in dimensionsList:
-    length, width, height = re.split("x", dimension)
-    
-    side1 = int(length) * int(width)
-    side2 = int(width) * int(height)
-    side3 = int(height) * int(length)
+        surfaceArea = 2 * side1 + 2 * side2 + 2 * side3
+        smallestSide = min(side1, side2, side3)
 
-    surfaceArea = 2 * side1 + 2 * side2 + 2 * side3
-    smallestSide = min(side1, side2, side3)
+        totalSquareFeet += surfaceArea + smallestSide
 
-    totalSquareFeet += calculateSurfAreaExtra(surfaceArea, smallestSide)
+    return totalSquareFeet
 
-print(totalSquareFeet)
+print("test1", calculatetotalSquareFeet(test1)) #58+  43 = 101
+print("result", calculatetotalSquareFeet(dimensionsList))
 
 file.close()
